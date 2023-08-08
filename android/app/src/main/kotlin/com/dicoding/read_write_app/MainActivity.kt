45aced4fb4a736368d3b9da0f23f2951a38d9722
Musthafa Faishal <musthafafaishal@gmail.com>
2023-08-09 01:03:12 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/dicoding/read_write_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dicoding.read_write_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/file_helper.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+class FileHelper {
+  static Future<File> writeFile(String path, String content) async {
+    final file = File(path);
+    print('Saved to $path');
+    return file.writeAsString(content);
+  }
+
+  static Future<String> readFile(String filePath) async {
+    try {
+      final file = File(filePath);
+      String contents = await file.readAsString();
+      return contents;
+    } catch (e) {
+      print(e);
+      return '';
+    }
+  }
+
+  static Future<String> getFilePath(String filename) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final prefix = directory.path;
+    final absolutePath = '$prefix/$filename.txt';
+    return absolutePath;
+  }
+}
